db02def86907ebd15e4a35a6fed51027c78acbe2
gauths <gauths@USHYDGAUTHS6>
2023-03-21 00:43:27 +0530

    Add project files.

---
+++ Project1/Models/Course.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Project1.Models {
+    public class Course {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public int courseID { get; set; }
+
+        [Required]
+        public string courseName { get; set; }
+        public DateTime courseDuration { get; set; }
+
+        [Required]
+        public float courseFee { get; set; }
+
+        [Required]
+        public bool status { get; set; }
+
+        public ICollection<Enrollment> enrollments { get; set; }
+    }
+}
---
+++ Project1/Models/SMSDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project1.Models {
+    public class SMSDBContext : DbContext {
+
+        public SMSDBContext(DbContextOptions<SMSDBContext> options) : base(options)
+        {
+        }
+        public DbSet<Admin> Admins { get; set; }
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
